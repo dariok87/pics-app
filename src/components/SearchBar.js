87f41ac2072738bf7598af0ce69f7833c78c1f08
dariok87 <dkemenovic@gmail.com>
2022-03-12 13:52:52 +0100

    Create input event handler

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,14 +1,18 @@
 import React from "react";
 
 class SearchBar extends React.Component {
+  onInputChange(event) { //this method is called anytime someone calls input
+    console.log(event.target.value);
+  }
+
   render() {
     return (
       <div className="ui segment">
         <form className="ui form">
           <div className="field">
             <label>Image Search</label>
-            <input type='text' />
-          </div>
+            <input type='text'onChange={this.onInputChange} /> 
+          </div> 
         </form>
       </div>
     );
