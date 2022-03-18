db39ab423152e7dda3bf86d172961ead21b51cba
dariok87 <dkemenovic@gmail.com>
2022-03-18 18:25:18 +0100

    Rename callback function

--- src/components/App.js
+++ src/components/App.js
@@ -2,14 +2,14 @@ import React from "react";
 import SearchBar from "./SearchBar";
 
 class App extends React.Component {
-  onSearchSubmit(term) { // It will be used for fetching images, api request
+  onClickSearchSubmit(term) { // It will be used for fetching images, api request
     console.log(term);
   }
 
   render() {
     return (
       <div className='ui container' style={{ marginTop: '10px'}}>
-        <SearchBar onSubmit={this.onSearchSubmit} /> {/* It does not have to be called onSubmit*/}
+        <SearchBar onSubmit={this.onClickSearchSubmit} /> {/* It does not have to be called onSubmit*/}
       </div>
     );
   }
