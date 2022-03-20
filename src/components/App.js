14613c2d18128c43eb7342c2149f86363e2572a4
dariok87 <dkemenovic@gmail.com>
2022-03-20 20:49:38 +0100

    View requested data

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,16 @@
 import React from "react";
+import axios from "axios";
 import SearchBar from "./SearchBar";
+
 
 class App extends React.Component {
   onClickSearchSubmit(term) { // It will be used for fetching images, api request
-    console.log(term);
+    axios.get('https://api.unsplash.com/search/photos', {
+      params: { query: term},
+      headers: {
+        Authorization: 'Client-ID' FL3iIEC7RqreG5Ij2so5jGlZ77KUNOK3SLf5QZ4Bu5M
+      }
+    });
   }
 
   render() {
