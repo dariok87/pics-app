d455b23a582ef28bfbb3cf40784ccb1ad5bc0631
dariok87 <dkemenovic@gmail.com>
2022-03-21 21:01:20 +0100

    direct access to list of images

--- src/components/App.js
+++ src/components/App.js
@@ -11,8 +11,8 @@ class App extends React.Component {
         Authorization: 
           'Client-ID FL3iIEC7RqreG5Ij2so5jGlZ77KUNOK3SLf5QZ4Bu5M'
       }
-    }).then((response) => {
-        console.log(response);
+    }).then((response) => { 
+        console.log(response.data.results);
     });
   }
 
